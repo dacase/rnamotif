295f3e058287ef17d988ae74de0fe23701faa4cb
macke <macke>
1998-12-14 04:32:30 +0000

    added parse tree stuff: T_*, C_* NODE_T.  VALUE_T has value and type now.

--- src/rnamot.h
+++ src/rnamot.h
@@ -2,10 +2,21 @@
 #define	__RNAMOT__
 
 #define	UNDEF	(-1)
-#define	LONGEST	(-2)
+#define	LASTVAL	(-2)
+
+#define	T_UNDEF		0
+#define	T_INT		1
+#define	T_FLOAT		2
+#define	T_STRING	3
+#define	T_PAIR		4
+
+#define	C_UNDEF		0
+#define	C_LIT		1
+#define	C_VAR		2
+#define	C_EXPR		3
 
 typedef	struct	value_t	{
-	int	v_sym;
+	int	v_type;
 	union	{
 		int	v_ival;
 		float	v_fval;
@@ -13,6 +24,22 @@ typedef	struct	value_t	{
 		char	*v_pval;
 	} v_value;
 } VALUE_T;
+
+typedef	struct	ident_t	{
+	char	*i_name;
+	int	i_type;
+	int	i_class;
+	VALUE_T	i_value;
+} IDENT_T;
+
+typedef	struct	node_t	{
+	int	n_sym;
+	int	n_type;
+	int	n_class;
+	VALUE_T	n_val;
+	struct	node_t	*n_left;
+	struct	node_t	*n_right;
+} NODE_T;
 
 typedef	struct	strel_t	{
 	int	s_type;
