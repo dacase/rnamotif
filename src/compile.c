9127348059bb20c719a3d16250787f6a1774ef4c
macke <macke>
1998-12-15 04:42:49 +0000

    partial evaluator.  Want to think about it for a bit.

--- src/compile.c
+++ src/compile.c
@@ -4,32 +4,24 @@
 #include "rnamot.h"
 #include "y.tab.h"
 
-	/* These are the "contexts" that the parser operates under	*/
-	/* required, as several items have the same shape, ie descr's	*/
-	/* but are descr's in the descr section, but sites in the	*/
-	/* site section.						*/
-
-#define	CTX_START	0
-#define	CTX_PARM	1
-#define	CTX_DESCR	2
-#define	CTX_SITE	3
-#define	CTX_ERROR	4
-
-static	int	rmc_context = CTX_START;
-
 #define	VALSTKSIZE	20
 static	VALUE_T	valstk[ VALSTKSIZE ];
 static	int	n_valstk;
 
-static	IDENT_T	idtab[] = {
-	{ "wc", T_PAIR, C_VAR },
-	{ "gu", T_PAIR, C_VAR },
-	{ "tr", T_PAIR, C_VAR },
-	{ "qu", T_PAIR, C_VAR },
-	{ "overlap", T_INT, C_VAR },
-	{ "db", T_STRING, C_VAR }
+#define	GLOBAL_IDS_SIZE	50
+static	IDENT_T	global_ids[ GLOBAL_IDS_SIZE ] = {
+	{ "wc", T_PAIR, C_VAR, S_GLOBAL, { T_PAIR, NULL } },
+	{ "gu", T_PAIR, C_VAR, S_GLOBAL, { T_PAIR, NULL } },
+	{ "tr", T_PAIR, C_VAR, S_GLOBAL, { T_PAIR, NULL } },
+	{ "qu", T_PAIR, C_VAR, S_GLOBAL, { T_PAIR, NULL } },
+	{ "overlap", T_INT, C_VAR, S_GLOBAL, { T_INT, 0 } },
+	{ "db", T_STRING, C_VAR, S_GLOBAL, { T_STRING, NULL } }
 };
-static	int	n_idtab = sizeof( idtab ) / sizeof( IDENT_T );
+static	int	n_global_ids = 6;
+
+#define	LOCAL_IDS_SIZE	20
+static	IDENT_T	*local_ids[ LOCAL_IDS_SIZE ];
+static	int	n_local_ids;
 
 #define	DESCRSIZE 100
 static	STREL_T	descr[ DESCRSIZE ];
@@ -39,93 +31,82 @@ static	STREL_T	*stp;
 void	SE_dump();
 void	SE_dump_descr();
 
-void	set_context( sym )
-int	sym;
-{
+static	void	enter_id();
+static	IDENT_T	*find_id();
+static	void	eval();
 
-	switch( sym ){
-	case SYM_PARM :
-		if( rmc_context < CTX_PARM )
-			rmc_context = CTX_PARM;
-		else if( rmc_context == CTX_PARM ){
-			fprintf( stderr,
-		"set_context: FATAL: At most 1 `parm' section permitted.\n" );
-			exit( 1 );
-		}else if( rmc_context > CTX_PARM ){
-			fprintf( stderr,
-	"set_context: FATAL: section order is pair, `parm', descr, site.\n" );
-			exit( 1 );
-		}
-		break;
-	case SYM_DESCR :
-		n_descr = 0;
-		if( rmc_context < CTX_DESCR )
-			rmc_context = CTX_DESCR;
-		else if( rmc_context == CTX_DESCR ){
-			fprintf( stderr,
-		"set_context: FATAL: At most 1 `descr' section permitted.\n" );
-			exit( 1 );
-		}else if( rmc_context > CTX_DESCR ){
-			fprintf( stderr,
-	"set_context: FATAL: section order is pair, parm, `descr', site.\n" );
-			exit( 1 );
-		}
-		break;
-	case SYM_SITE :
-		if( rmc_context == CTX_DESCR )
-			rmc_context = CTX_SITE;
-		else if( rmc_context == CTX_SITE ){
-			fprintf( stderr,
-		"set_context: FATAL: At most 1 `site' section permitted.\n" );
-			exit( 1 );
-		}else if( rmc_context < CTX_DESCR ){
-			fprintf( stderr,
-	"set_context: FATAL: section order is pair, parm, descr, `site'.\n" );
-			exit( 1 );
-		}
-		break;
-	default :
-		fprintf( stderr,
-			"set_context: FATAL: unexpected symbol: %d.\n", sym );
-		exit( 1 );
-		break;
-	}
-}
-
-void	SE_new( stype )
+void	SE_open( stype )
 int	stype;
 {
+	VALUE_T	val;
 
 	n_valstk = 0;
-	if( rmc_context == CTX_DESCR ){
-		if( n_descr == DESCRSIZE ){
-			fprintf( stderr,
-			"SE_new: FATAL: descr array size(%d) exceeded.\n",
-				DESCRSIZE );
-			exit( 1 );
-		}
-		stp = &descr[ n_descr ];
-		n_descr++;
-		stp->s_type = stype;
-		stp->s_index = n_descr - 1;
-		stp->s_tag = NULL;
-		stp->s_next = NULL;
-		stp->s_pairs = NULL;
-		stp->s_minlen = UNDEF;
-		stp->s_maxlen = UNDEF;
-		stp->s_seq = NULL;
-		stp->s_mismatch = 0;
-		stp->s_mispair = 0;
-		stp->s_pairdata = NULL;
-		stp->s_sites = NULL;
+	if( n_descr == DESCRSIZE ){
+		fprintf( stderr,
+		"SE_new: FATAL: descr array size(%d) exceeded.\n",
+			DESCRSIZE );
+		exit( 1 );
 	}
+	stp = &descr[ n_descr ];
+	n_descr++;
+	stp->s_type = stype;
+	stp->s_index = n_descr - 1;
+	stp->s_tag = NULL;
+	stp->s_next = NULL;
+	stp->s_pairs = NULL;
+	stp->s_minlen = UNDEF;
+	stp->s_maxlen = UNDEF;
+	stp->s_seq = NULL;
+	stp->s_mismatch = 0;
+	stp->s_mispair = 0;
+	stp->s_pairdata = NULL;
+	stp->s_sites = NULL;
+	
+	n_local_ids = 0;
+	val.v_type = T_STRING;
+	val.v_value.v_pval = NULL;
+	enter_id( "tag", T_STRING, C_VAR, S_LOCAL, &val );
+
+	val.v_type = T_INT;
+	val.v_value.v_ival = UNDEF;
+	enter_id( "minlen", T_INT, C_VAR, S_LOCAL, &val );
+
+	val.v_type = T_INT;
+	val.v_value.v_ival = UNDEF;
+	enter_id( "maxlen", T_INT, C_VAR, S_LOCAL, &val );
+
+	val.v_type = T_STRING;
+	val.v_value.v_pval = NULL;
+	enter_id( "seq", T_STRING, C_VAR, S_LOCAL, &val );
+
+	val.v_type = T_INT;
+	val.v_value.v_ival = 0;
+	enter_id( "mismatch", T_INT, C_VAR, S_LOCAL, &val );
+
+	val.v_type = T_INT;
+	val.v_value.v_ival = 0;
+	enter_id( "mispair", T_INT, C_VAR, S_LOCAL, &val );
+
+	val.v_type = T_PAIR;
+	val.v_value.v_pval = NULL;
+	enter_id( "pair", T_PAIR, C_VAR, S_LOCAL, &val );
 }
 
 void	SE_addval( expr )
 NODE_T	*expr;
 {
+	int	op;
+	NODE_T	*npl, *npr;
+	char	*id;
+	char	*sp;
 
-	dumpexpr( stderr, expr, 0 );
+	n_valstk = 0;
+	eval( expr, 0 );
+}
+
+void	SE_close()
+{
+
 }
 
 void	SE_dump( fp, d_pair, d_parm, d_descr, d_site )
@@ -212,4 +193,171 @@ STREL_T	*stp;
 	fprintf( fp, "\tseq  = %s\n",
 		stp->s_seq ? stp->s_seq : "(No seq)" );
 	fprintf( fp, "}\n" );
+}
+
+static	void	enter_id( name, type, class, scope, vp )
+char	name[];
+int	type;
+int	class;
+VALUE_T	*vp;
+{
+	IDENT_T	*ip;
+	char	*np;
+
+	if( scope == S_GLOBAL ){
+		if( n_global_ids >= GLOBAL_IDS_SIZE ){
+			fprintf( stderr,
+			"enter_id: FATAL: global symbol tab overflow.\n" );
+			exit( 1 );
+		}
+		ip = &global_ids[ n_global_ids ];
+		n_global_ids++;
+	}else{
+		if( n_local_ids >= LOCAL_IDS_SIZE ){
+			fprintf( stderr,
+			"enter_id: FATAL: local symbol tab overflow.\n" );
+			exit( 1 );
+		}
+		ip = ( IDENT_T * )malloc( sizeof( IDENT_T ) );
+		if( ip == NULL ){
+			fprintf( stderr,
+				"enter_id: FATAL: can't alloc local ip.\n" );
+			exit( 1 );
+		}
+		local_ids[ n_local_ids ] = ip;
+		n_local_ids++;
+	}
+	np = ( char * )malloc( strlen( name ) + 1 );
+	if( np == NULL ){
+		fprintf( stderr,
+			"enter_id: FATAL: can't alloc np for name.\n" );
+		exit( 1 );
+	}
+	strcpy( np, name );
+	ip->i_name = np;
+	ip->i_type = type;
+	ip->i_class = class;
+	ip->i_scope = scope;
+	ip->i_val.v_type = type;
+	if( type == T_INT ){
+		ip->i_val.v_value.v_ival = vp->v_value.v_ival;
+	}else if( type == T_STRING ){
+		if( vp->v_value.v_pval == NULL ) 
+			ip->i_val.v_value.v_pval = NULL;
+		else{
+			np = ( char * )
+				malloc(strlen(vp->v_value.v_pval)+1);
+			if( np == NULL ){
+				fprintf( stderr,
+			"enter_id: FATAL: can't alloc np for string val.\n" );
+				exit( 1 );
+			}
+			strcpy( np, vp->v_value.v_pval );
+			ip->i_val.v_value.v_pval = np;
+		}
+	}else if( type == T_PAIR ){
+		ip->i_val.v_value.v_pval = NULL;
+	}
+}
+
+static	IDENT_T	*find_id( name )
+char	name[];
+{
+	int	i;
+	IDENT_T	*ip;
+	
+	for( i = 0; i < n_local_ids; i++ ){
+		ip = local_ids[ i ];
+		if( !strcmp( name, ip->i_name ) )
+			return( ip );
+	}
+	for( ip = global_ids, i = 0; i < n_global_ids; i++, ip++ ){
+		if( !strcmp( name, ip->i_name ) )
+			return( ip );
+	}
+	return( NULL );
+}
+
+static	void	eval( expr )
+NODE_T	*expr;
+{
+	char	*sp;
+	IDENT_T	*ip;
+	int	l_type, r_type;
+
+	if( expr ){
+		eval( expr->n_left );
+		eval( expr->n_right );
+		switch( expr->n_sym ){
+		case SYM_INT :
+			valstk[ n_valstk ].v_type = T_INT;
+			valstk[ n_valstk ].v_value.v_ival =
+				expr->n_val.v_value.v_ival;
+			n_valstk++;
+			break;
+		case SYM_STRING :
+			sp = ( char * )
+				malloc(strlen( expr->n_val.v_value.v_pval )+1);
+			if( sp == NULL ){
+				fprintf( stderr,
+			"eval: FATAL: can't allocate sp for string.\n" );
+				exit( 1 );
+			}
+			strcpy( sp, expr->n_val.v_value.v_pval );
+			valstk[ n_valstk ].v_type = T_STRING;
+			valstk[ n_valstk ].v_value.v_pval = sp;
+			n_valstk++;
+			break;
+		case SYM_IDENT :
+			ip = find_id( expr->n_val.v_value.v_pval );
+			if( ip == NULL ){
+				fprintf( stderr,
+					"eval: FATAL: unknown id '%s'.\n",
+					expr->n_val.v_value.v_pval );
+				exit( 1 );
+			}
+			valstk[ n_valstk ].v_type = ip->i_type;
+			valstk[ n_valstk ].v_value.v_pval = ip;
+			n_valstk++;
+			break;
+		case SYM_PLUS :
+			l_type = valstk[ n_valstk - 2 ].v_type;
+			r_type = valstk[ n_valstk - 1 ].v_type;
+			if( l_type != r_type ){
+				fprintf( stderr,
+					"eval: FATAL: type mismatch '+'\n" );
+				exit( 1 );
+			}
+			if( l_type == T_INT ){
+				valstk[ n_valstk - 2 ].v_value.v_ival +=
+					valstk[ n_valstk - 1 ].v_value.v_ival;
+			}else if( l_type == T_STRING ){
+			}else if( l_type == T_PAIR ){
+			}
+			n_valstk--;
+			break;
+		case SYM_MINUS :
+			l_type = valstk[ n_valstk - 2 ].v_type;
+			r_type = valstk[ n_valstk - 1 ].v_type;
+			if( l_type != r_type ){
+				fprintf( stderr,
+					"eval: FATAL: type mismatch '-'\n" );
+				exit( 1 );
+			}
+			if( l_type == T_INT ){
+				valstk[ n_valstk - 2 ].v_value.v_ival -=
+					valstk[ n_valstk - 1 ].v_value.v_ival;
+			}else if( l_type == T_STRING ){
+			}else if( l_type == T_PAIR ){
+			}
+			n_valstk--;
+			break;
+		case SYM_ASSIGN :
+			break;
+		case SYM_PLUS_ASSIGN :
+			break;
+		case SYM_MINUS_ASSIGN :
+			break;
+		}
+	}
 }
