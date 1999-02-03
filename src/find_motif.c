02cc8230ee89915194b7b92830afda511ed7a0bb
macke <macke>
1999-02-03 05:44:08 +0000

    modified parm list to accept the seq & slen.

--- src/find_motif.c
+++ src/find_motif.c
@@ -2,11 +2,12 @@
 
 #include "rnamot.h"
 
-int	find_rnamot( fp, n_descr, descr, sites )
-FILE	*fp;
+int	find_rnamot( n_descr, descr, sites, slen, sbuf )
 int	n_descr;
 STREL_T descr[];
 SITE_T	*sites;
+int	slen;
+char	sbuf[];
 {
 
 }
