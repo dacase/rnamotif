3d9dbbf0ded4b8500003fbf3b58be23747be0622
macke <macke>
1999-02-06 21:21:44 +0000

    get the lengths of the bits in order to determine where the search should occur.

--- src/find_motif.c
+++ src/find_motif.c
@@ -2,12 +2,24 @@
 
 #include "rnamot.h"
 
-int	find_rnamot( n_descr, descr, sites, slen, sbuf )
+static	int	motiflen();
+
+int	find_motif( n_descr, descr, sites, slen, sbuf )
 int	n_descr;
 STREL_T descr[];
 SITE_T	*sites;
 int	slen;
 char	sbuf[];
+{
+	int	mlen;
+	
+	mlen = motiflen( n_descr, descr, slen );
+}
+
+static	int	motiflen( n_descr, descr, slen )
+int	n_descr;
+STREL_T	descr[];
+int	slen;
 {
 
 }
