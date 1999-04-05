1d8f8c912806b36a691f4e3cf17af28e75b7cef2
macke <macke>
1999-04-05 01:06:36 +0000

    moved print_limits() from find_motif.c to here. Renamed to print_hierarchy().

--- src/dump.c
+++ src/dump.c
@@ -19,15 +19,22 @@ void	RM_dump_pair();
 void	RM_dump_descr();
 void	RM_dump_sites();
 
-void	RM_dump( fp, d_parms, d_descr, d_sites )
+static	void	print_hierarchy();
+static	void	print_1_element();
+static	void	mk_prefix();
+static	void	mk_strel_name();
+
+void	RM_dump( fp, d_parms, d_descr, d_sites, d_hierarchy )
 FILE	*fp;
 int	d_parms;
 int	d_descr;
 int	d_sites;
+int	d_hierarchy;
 {
 	int	i;
 	IDENT_T	*ip;
 	STREL_T	*stp;
+	char	prefix[ 100 ];
 
 	if( d_parms ){
 		fprintf( fp, "PARMS: %3d global symbols.\n", rm_n_global_ids );
@@ -42,6 +49,12 @@ int	d_sites;
 	}
 	if( d_sites ){
 		RM_dump_sites( fp );
+	}
+	if( d_hierarchy ){
+		fprintf( fp,
+	"descr minl  maxl  mngl  mxgl  mnil  mxil start  stop  descr\n" );
+		strcpy( prefix, "+" );
+		print_hierarchy( fp, 0, prefix, 0, rm_descr );
 	}
 }
 
@@ -460,5 +473,188 @@ FILE	*fp;
 		RM_dump_pairset( fp, sp->s_pairset );
 		fprintf( fp, "\n" );
 		fprintf( fp, "}\n" );
+	}
+}
+
+static	void	print_hierarchy( fp, lev, prefix, fd, descr )
+FILE	*fp;
+int	lev;
+char	prefix[];
+int	fd;
+STREL_T	descr[];
+{
+	int	d, nd, s;
+	STREL_T	*stp, *stp1, *stp2;
+	char	prefix1[ 100 ], prefix2[ 100 ];;
+
+	for( d = fd; ; d = nd ){
+		stp = &descr[ d ];
+		print_1_element( fp, prefix, stp );
+		mk_prefix( stp, prefix, prefix1 ); 
+		for( s = 1; s < stp->s_n_scopes; s++ ){
+			stp1 = stp->s_scopes[ s - 1 ];
+			stp2 = stp->s_scopes[ s ];
+			print_hierarchy( fp, lev+2, prefix1, 
+				stp1->s_index+1, descr );
+			mk_prefix( stp2, prefix, prefix2 ); 
+			print_1_element( fp, prefix2, stp2 );
+		}
+		stp1 = stp->s_next;
+		if( stp1 == NULL )
+			return;
+		else
+			nd = stp1->s_index;
+	} 
+}
+
+static	void	print_1_element( fp, prefix, stp )
+FILE	*fp;
+char	prefix[];
+STREL_T	*stp;
+{
+	int	start, stop, l2r;
+	char	name[ 20 ], tstr[ 20 ];
+	char	*bp, buf[ 200 ];
+
+	bp = buf;
+	sprintf( bp, "%4d", stp->s_index );
+	bp += strlen( bp );
+
+	sprintf( bp, " %5d", stp->s_minlen );
+	bp += strlen( bp );
+
+	if( stp->s_maxlen == UNBOUNDED )
+		sprintf( bp, " UNBND" );
+	else
+		sprintf( bp, " %5d", stp->s_maxlen );
+	bp += strlen( bp );
+
+	if( stp->s_minglen == UNDEF )
+		sprintf( bp, " UNDEF" );
+	else
+		sprintf( bp, " %5d", stp->s_minglen );
+	bp += strlen( bp );
+
+	if( stp->s_maxglen == UNBOUNDED )
+		sprintf( bp, " UNBND" );
+	else if( stp->s_maxglen == UNDEF )
+		sprintf( bp, " UNDEF" );
+	else
+		sprintf( bp, " %5d", stp->s_maxglen );
+	bp += strlen( bp );
+
+	if( stp->s_minilen == UNDEF )
+		sprintf( bp, " UNDEF" );
+	else
+		sprintf( bp, " %5d", stp->s_minilen );
+	bp += strlen( bp );
+
+	if( stp->s_maxilen == UNBOUNDED )
+		sprintf( bp, " UNBND" );
+	else if( stp->s_maxilen == UNDEF )
+		sprintf( bp, " UNDEF" );
+	else
+		sprintf( bp, " %5d", stp->s_maxilen );
+	bp += strlen( bp );
+
+	sprintf( tstr, "%s%d", !stp->s_start.a_l2r ? "$-" : "",
+		stp->s_start.a_offset );
+	sprintf( bp, " %5s", tstr );
+	bp += strlen( bp );
+
+	sprintf( tstr, "%s%d", !stp->s_stop.a_l2r ? "$-" : "",
+		stp->s_stop.a_offset );
+	sprintf( bp, " %5s", tstr );
+	bp += strlen( bp );
+
+	mk_strel_name( stp, name );
+	sprintf( bp, "  %s%s", prefix, name );
+	bp += strlen( bp );
+
+	if( stp->s_scope == 0 ){
+		sprintf( bp, "+--+" );
+		bp += strlen( bp );
+	}
+	sprintf( bp, "\n" );
+	bp += strlen( bp );
+
+	fputs( buf, fp );
+}
+
+static	void	mk_prefix( stp, prefix, prefix1 )
+STREL_T	*stp;
+char	prefix[];
+char	prefix1[];
+{
+	char	*pp, *pp1;
+	int	plen;
+	STREL_T	*stp0;
+	int	inner, next, first;
+
+	strcpy( prefix1, prefix );
+	plen = strlen( prefix1 );
+	pp = &prefix1[ plen - 1 ];
+
+	inner = stp->s_inner != NULL;
+	if( inner )
+		first = stp->s_scope == 0;
+	else
+		first = 0;
+	next = stp->s_next != NULL;
+	if( next )
+		*pp = '|';
+	else if( *pp != '|' )
+		*pp = ' ';
+	if( first )
+		strcat( prefix1, "  |" );
+	strcat( prefix1, "  +" );
+}
+
+static	void	mk_strel_name( stp, name )
+STREL_T	*stp;
+char	name[];
+{
+
+	switch( stp->s_type ){
+	case SYM_SS :
+		strcpy( name, "ss" );
+		break;
+
+	case SYM_H5 :
+		strcpy( name, "h5" );
+		break;
+	case SYM_H3 :
+		strcpy( name, "h3" );
+		break;
+
+	case SYM_P5 :
+		strcpy( name, "p5" );
+		break;
+	case SYM_P3 :
+		strcpy( name, "p3" );
+		break;
+
+	case SYM_T1 :
+		strcpy( name, "t1" );
+		break;
+	case SYM_T2 :
+		strcpy( name, "t2" );
+		break;
+	case SYM_T3 :
+		strcpy( name, "t3" );
+		break;
+
+	case SYM_Q1 :
+		strcpy( name, "q1" );
+		break;
+	case SYM_Q2 :
+		strcpy( name, "q2" );
+		break;
+	case SYM_Q3 :
+		strcpy( name, "q3" );
+		break;
+	case SYM_Q4 :
+		strcpy( name, "q4" );
+		break;
 	}
 }
