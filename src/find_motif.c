1147caf598e08b9cbc5af0b1a4da2a74e6eb3cdf
macke <macke>
1999-04-29 03:39:23 +0000

    began computing the actual search locations, including the window.

--- src/find_motif.c
+++ src/find_motif.c
@@ -11,11 +11,15 @@ static	char	fm_emsg[ 256 ];
 static	char	*fm_locus;
 static	char	fm_slen;
 static	char	*fm_sbuf;
+static	int	*fm_window;
+static	int	fm_windowsize;
 
 static	int	find_motif();
 static	int	find_ss();
 static	int	find_wchlx();
 static	int	find_pknot();
+
+IDENT_T	*find_id();
 
 int	find_motif_driver( n_descr, descr, sites, locus, slen, sbuf )
 int	n_descr;
@@ -25,16 +29,46 @@ char	locus[];
 int	slen;
 char	sbuf[];
 {
-	int	i;
+	int	i, s_zero, s_dollar;
+	IDENT_T	*ip;
 	
+	if( fm_window == NULL ){
+		ip = find_id( "windowsize" );
+		if( ip == NULL )
+			errormsg( 1,
+				"find_motif_driver: windowsize undefined." );
+	
+			if( ip->i_val.v_value.v_ival <= 0 )
+				errormsg( 1,
+					"find_motif_driver: windowsize <= 0." );
+		else
+			fm_windowsize = ip->i_val.v_value.v_ival;
+		fm_window = ( int * )malloc( fm_windowsize * sizeof( int ) );
+		if( fm_window == NULL )
+			errormsg( 1,
+				"find_motif_driver: can't allocate fm_window.");
+	}
+
 fprintf( stderr, "fmd: locus = %s, slen = %d\n", locus, slen );
 
 	fm_locus = locus;
 	fm_slen = slen;
 	fm_sbuf = sbuf;
 
-	for( i = 0; i < slen - rm_dminlen; i++ ){
+	for( s_zero = 0; s_zero < slen - fm_windowsize; s_zero++ ){
+		s_dollar = s_zero + fm_windowsize - 1;
+fprintf( stderr, "srch.1: %4d:%4d\n", s_zero, s_dollar );
+/*
 		find_motif( 0, descr, i, i + rm_dmaxlen - 1 );
+*/
+	}
+
+	s_dollar = slen - 1;
+	for( i = 0; i <= slen - rm_dminlen; i++, s_zero++ ){
+fprintf( stderr, "srch.2: %4d:%4d\n", s_zero, s_dollar );
+/*
+		find_motif( 0, descr, i, i + rm_dmaxlen - 1 );
+*/
 	}
 
 	return( 0 );
