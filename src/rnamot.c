3b0abf553bd212c6a5e731257c27136758e68e97
macke <macke>
1999-06-20 02:13:59 +0000

    about to redo the whole database thing, but need to think a bit more about it.

--- src/rnamot.c
+++ src/rnamot.c
@@ -5,10 +5,12 @@
 #include "dbase.h"
 
 extern	int	rm_error;
-extern	char	rm_fname[];
+extern	char	rm_dfname[];
 extern	int	rm_copt;
 extern	int	rm_dopt;
 extern	int	rm_hopt;
+extern	FILE	*rm_dbfp;
+extern	int	rm_dtype;
 
 extern	STREL_T	rm_descr[];
 extern	int	rm_n_descr;
@@ -23,8 +25,11 @@ extern	int		rm_n_searches;
 #define	SBUF_SIZE	2000000
 static	char	sbuf[ SBUF_SIZE ];
 static	int	slen;
+static	char	csbuf[ SBUF_SIZE ];
 
 IDENT_T	*find_id();
+
+static	void	mk_rcmp();
 
 main( argc, argv )
 int	argc;
@@ -35,7 +40,8 @@ char	*argv[];
 	char	*dbnp;
 	char	locus[ 20 ];
 
-	RM_init( argc, argv );
+	if( RM_init( argc, argv ) )
+		exit( 1 );
 
 	if( yyparse() ){
 		errormsg( 1, "syntax error." );
@@ -44,7 +50,7 @@ char	*argv[];
 		if( SE_link( rm_n_descr, rm_descr ) )
 			exit( 1 );
 		fprintf( stderr, "%s: complete descr length: min/max = %d/",
-			rm_fname, rm_dminlen );
+			rm_dfname, rm_dminlen );
 		if( rm_dmaxlen == UNBOUNDED )
 			fprintf( stderr, "UNBND\n" );
 		else
@@ -70,10 +76,54 @@ char	*argv[];
 	for( dbp = DB_open( dbnp ); dbp; ){
 		if( slen = DB_getseq( dbp, locus, SBUF_SIZE, sbuf ) ){
 			find_motif_driver( rm_n_searches, rm_searches, rm_sites,
-				locus, slen, sbuf );
+				locus, 0, slen, sbuf );
+			mk_rcmp( slen, sbuf, csbuf );
+			find_motif_driver( rm_n_searches, rm_searches, rm_sites,
+				locus, 1, slen, csbuf );
 		}
 		dbp = DB_next( dbp );
 	}
+/*
+	while( slen = DB_getseq( stdin, locus, SBUF_SIZE, sbuf ) ){
+		find_motif_driver( rm_n_searches, rm_searches, rm_sites,
+			locus, 0, slen, sbuf );
+		mk_rcmp( slen, sbuf, csbuf );
+		find_motif_driver( rm_n_searches, rm_searches, rm_sites,
+			locus, 1, slen, csbuf );
+	}
+*/
 
 	exit( 0 );
+}
+
+static	void	init_rcmp()
+{
+	int	i;
+
+}
+
+static	void	mk_rcmp( slen, sbuf, csbuf )
+int	slen;
+char	sbuf[];
+char	csbuf[];
+{
+	static	int	init = 0;
+	static	char	wc_cmp[ 128 ];
+	char	*sp, *cp;
+	int	i;
+
+	if( !init ){
+		init = 1;
+		for( i = 0; i < 128; i++ )
+			wc_cmp[ i ] = 'n';
+		wc_cmp[ 'a' ] = 't'; wc_cmp[ 'A' ] = 't';
+		wc_cmp[ 'c' ] = 'g'; wc_cmp[ 'C' ] = 'g';
+		wc_cmp[ 'g' ] = 'c'; wc_cmp[ 'G' ] = 'c';
+		wc_cmp[ 't' ] = 'a'; wc_cmp[ 'T' ] = 'a';
+		wc_cmp[ 'u' ] = 'a'; wc_cmp[ 'U' ] = 'a';
+	}
+
+	csbuf[ slen ] = '\0';
+	for( sp = sbuf, cp = &csbuf[ slen - 1 ], i = 0; i < slen; i++ )
+		*cp-- = wc_cmp[ *sp++ ];
 }
