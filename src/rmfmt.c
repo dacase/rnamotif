4a0c96ec3e789f4691f9ee273a8144933aef8e4d
macke <macke>
1999-07-08 03:14:33 +0000

    added code that reads a header #RM fmt d1 d2 ... dN and left justifies all but h3, t2, q2 and q4 which are right justified.  Useful? Maybe, maybe not.

--- src/rmfmt.c
+++ src/rmfmt.c
@@ -3,6 +3,15 @@
 
 #define	MAXFIELDS	200
 #define	FIELD1		4
+static	char	*fields[ MAXFIELDS ];
+static	int	n_fields, t_fields;
+
+static	int	*maxw;
+
+#define	FMT_LEFT	0
+#define	FMT_RIGHT	1
+static	int	*fmt;
+static	int	*getfmt();
 
 main( argc, argv )
 int	argc;
@@ -12,9 +21,7 @@ char	*argv[];
 	char	*tfnp1, *tfnp2;
 	char	cmd[ 256 ];
 	char	line[ 1024 ];
-	char	*fields[ MAXFIELDS ];
-	int	f, n_fields, t_fields;
-	int	*maxw, fw, fs;
+	int	f, fw, fs;
 
 	if( argc == 1 )
 		ifp = stdin;
@@ -36,6 +43,17 @@ char	*argv[];
 
 	for( t_fields = 0; fgets( line, sizeof( line ), ifp ); ){
 		n_fields = split( line, fields, " \t\n" );
+		if( n_fields == 0 )
+			continue;
+		else if( !strcmp( fields[ 0 ], "#RM" ) ){
+			if( n_fields > 1 && !strcmp( fields[ 1 ], "fmt" ) ){
+				fmt = getfmt( n_fields, fields );
+				if( fmt == NULL )
+					exit( 1 );
+			}
+			continue;
+		}else if( *fields[ 0 ] == '#' )
+			continue;
 		if( t_fields == 0 ){
 			t_fields = n_fields;
 			maxw = ( int * )
@@ -75,9 +93,16 @@ char	*argv[];
 			fields[ 0 ], fields[ 1 ], fields[ 2 ], fields[ 3 ] );
 		for( f = 0; f < t_fields - FIELD1; f++ ){
 			fw = strlen( fields[ FIELD1 + f ] );
-			printf( " %s", fields[ FIELD1 + f ] );
-			for( fs = 0; fs < maxw[ f ] - fw; fs++ )
-				putchar( ' ' );
+			if( fmt[ f ] == FMT_LEFT ){
+				printf( " %s", fields[ FIELD1 + f ] );
+				for( fs = 0; fs < maxw[ f ] - fw; fs++ )
+					putchar( ' ' );
+			}else{
+				printf( " " );
+				for( fs = 0; fs < maxw[ f ] - fw; fs++ )
+					putchar( ' ' );
+				printf( "%s", fields[ FIELD1 + f ] );
+			}
 		}
 		printf( "\n" );
 		for( f = 0; f < n_fields; f++ )
@@ -92,4 +117,28 @@ char	*argv[];
 		fclose( ifp );
 
 	exit( 0 );
+}
+
+static	int	*getfmt( n_fields, fields )
+int	n_fields;
+char	*fields[];
+{
+	int	f;
+	int	*fmt;
+
+	fmt = ( int * )malloc( ( n_fields - 2 ) * sizeof( int ) );
+	if( fmt == NULL ){
+		fprintf( stderr, "getfmt: can't allocate fmt.\n" );
+		return( NULL );
+	}
+	for( f = 2; f < n_fields; f++ ){
+		if( !strcmp( fields[ f ], "h3" ) ||
+				!strcmp( fields[ f ], "t2" ) ||
+				!strcmp( fields[ f ], "q2" ) ||
+				!strcmp( fields[ f ], "q4" ) )
+			fmt[ f - 2 ] = FMT_RIGHT;
+		else
+			fmt[ f - 2 ] = FMT_LEFT;
+	}
+	return( fmt );
 }
