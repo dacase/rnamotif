9be1425fd42a913b53ac7d39ba75d871b9972310
macke <macke>
1999-09-04 23:26:59 +0000

    added SYM_CALL; replaced literal case interior with #include of dumpnode.h which is created from y.tab. by mk_dumpnode.h

--- src/node.c
+++ src/node.c
@@ -46,6 +46,10 @@ NODE_T	*right;
 		np->n_class = C_LIT;
 		np->n_val.v_type = T_POS;
 		np->n_val.v_value.v_pval = vp->v_value.v_pval;
+	}else if( sym == SYM_CALL ){
+		np->n_val.v_type = T_STRING;
+		np->n_val.v_value.v_pval = left->n_val.v_value.v_pval;
+		np->n_left = NULL;
 	}
 	return( np );
 }
@@ -89,98 +93,8 @@ int	indent;
 		np, np->n_left, np->n_right,
 		np->n_lineno, np->n_type, np->n_class );
 	switch( np->n_sym ){
-	case SYM_PARMS :
-		fprintf( fp, "SYM_PARMS\n" );
-		break;
-	case SYM_DESCR :
-		fprintf( fp, "SYM_DESCR\n" );
-		break;
-	case SYM_SITES :
-		fprintf( fp, "SYM_SITES\n" );
-		break;
-	case SYM_H5 :
-		fprintf( fp, "SYM_H5\n" );
-		break;
-	case SYM_H3 :
-		fprintf( fp, "SYM_H3\n" );
-		break;
-	case SYM_P5 :
-		fprintf( fp, "SYM_P5\n" );
-		break;
-	case SYM_P3 :
-		fprintf( fp, "SYM_P3\n" );
-		break;
-	case SYM_T1 :
-		fprintf( fp, "SYM_T1\n" );
-		break;
-	case SYM_T2 :
-		fprintf( fp, "SYM_T2\n" );
-		break;
-	case SYM_T3 :
-		fprintf( fp, "SYM_T3\n" );
-		break;
-	case SYM_Q1 :
-		fprintf( fp, "SYM_Q1\n" );
-		break;
-	case SYM_Q2 :
-		fprintf( fp, "SYM_Q2\n" );
-		break;
-	case SYM_Q3 :
-		fprintf( fp, "SYM_Q3\n" );
-		break;
-	case SYM_Q4 :
-		fprintf( fp, "SYM_Q4\n" );
-		break;
-	case SYM_IDENT :
-		fprintf( fp, "SYM_IDENT = '%s'\n", np->n_val.v_value.v_pval );
-		break;
-	case SYM_INT :
-		fprintf( fp, "SYM_INT = %d\n", np->n_val.v_value.v_ival );
-		break;
-	case SYM_FLOAT :
-		fprintf( fp, "SYM_FLOAT = %e\n", np->n_val.v_value.v_fval );
-		break;
-	case SYM_STRING :
-		fprintf( fp, "SYM_STRING = '%s'\n", np->n_val.v_value.v_pval );
-		break;
-	case SYM_ASSIGN :
-		fprintf( fp, "SYM_ASSIGN\n" );
-		break;
-	case SYM_PLUS_ASSIGN :
-		fprintf( fp, "SYM_PLUS_ASSIGN\n" );
-		break;
-	case SYM_MINUS_ASSIGN :
-		fprintf( fp, "SYM_MINUS_ASSIGN\n" );
-		break;
-	case SYM_PLUS :
-		fprintf( fp, "SYM_PLUS\n" );
-		break;
-	case SYM_MINUS :
-		fprintf( fp, "SYM_MINUS\n" );
-		break;
-	case SYM_DOLLAR :
-		fprintf( fp, "SYM_DOLLAR\n" );
-		break;
-	case SYM_LPAREN :
-		fprintf( fp, "SYM_LPAREN\n" );
-		break;
-	case SYM_RPAREN :
-		fprintf( fp, "SYM_RPAREN\n" );
-		break;
-	case SYM_LCURLY :
-		fprintf( fp, "SYM_LCURLY\n" );
-		break;
-	case SYM_RCURLY :
-		fprintf( fp, "SYM_RCURLY\n" );
-		break;
-	case SYM_COMMA :
-		fprintf( fp, "SYM_COMMA\n" );
-		break;
-	case SYM_COLON :
-		fprintf( fp, "SYM_COLON\n" );
-		break;
-	case SYM_ERROR :
-		fprintf( fp, "SYM_ERROR\n" );
-		break;
+
+#include "dumpnode.h"
+
 	}
 }
