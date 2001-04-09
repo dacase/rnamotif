74dd56878617b5fc8cfdf94a0b177990fcfea5c0
case <case>
2001-04-09 17:01:24 +0000

    simplify

--- config.h
+++ config.h
@@ -1,14 +1,13 @@
 #  Edit the configuration variables below to match your system:
 
 CC= cc
+CFLAGS= -O2
 YACC = yacc
 LEX = lex
 
 # Uncomment & use these 3 lines to use Genbank in addition to FASTN:
-#GBHOME=		/home/macke/gensearch
-#GBLIB=		$(GBHOME)/libgb.a
-#CFLAGS=		-O2 -DUSE_GENBANK $(LOCAL_CFLAGS)
 
-# Uncomment & use this line for FASTN only
-CFLAGS= -O2
-#CFLAGS= -g 
+# GBHOME=/home/macke/gensearch
+# GBLIB=$(GBHOME)/libgb.a
+# CFLAGS=-O2 -DUSE_GENBANK
+
