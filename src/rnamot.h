66d9ea48cea2c17f608ca5ed3b4506b371d8f26b
case <case>
2001-04-23 20:53:11 +0000

    change the version number

--- src/rnamot.h
+++ src/rnamot.h
@@ -1,7 +1,7 @@
 #ifndef	__RNAMOT__
 #define	__RNAMOT__
 
-#define	VERSION	"v1.4.3 2001-mar-28"
+#define	VERSION	"v1.5.0 2001-apr-23"
 
 #define	U_MSG_S	\
 "usage: %s [ options ] descr [ seq-file ... ]\n\n\
